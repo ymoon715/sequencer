66e261346d15a3ba1213b9cb2ca2467aac9468ba
ymoon715 <ymoon715@gmail.com>
2018-10-14 18:31:54 -0700

    Updates

---
+++ precache-manifest.1cc04af7737721758899a0e4fad06697.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "46c08ecd3fc74dee1d172892dd49fff8",
+    "url": "/sequencer/static/media/Snare.46c08ecd.mp3"
+  },
+  {
+    "revision": "aa76f8aff6209463c374",
+    "url": "/sequencer/static/css/main.d48f131c.chunk.css"
+  },
+  {
+    "revision": "033a3c5d72dca47e4458",
+    "url": "/sequencer/static/js/1.033a3c5d.chunk.js"
+  },
+  {
+    "revision": "21b1cf13bc65110b79f7",
+    "url": "/sequencer/static/js/runtime~main.21b1cf13.js"
+  },
+  {
+    "revision": "e22b64e26d7fe0111bb8e64bf284e70b",
+    "url": "/sequencer/static/media/ClosedHiHat.e22b64e2.mp3"
+  },
+  {
+    "revision": "8fe135c2bd03054326eea700c78130b2",
+    "url": "/sequencer/static/media/OpenHiHat.8fe135c2.mp3"
+  },
+  {
+    "revision": "382936a3629b64f001073742e12a38e6",
+    "url": "/sequencer/static/media/Kick.382936a3.mp3"
+  },
+  {
+    "revision": "c0acb0e274a9b8573292f1b9e743f365",
+    "url": "/sequencer/static/media/Rim.c0acb0e2.mp3"
+  },
+  {
+    "revision": "aa76f8aff6209463c374",
+    "url": "/sequencer/static/js/main.aa76f8af.chunk.js"
+  },
+  {
+    "revision": "3857e4bfbc208fd589cee0dc653519dd",
+    "url": "/sequencer/static/media/Bass.3857e4bf.mp3"
+  },
+  {
+    "revision": "3ec5329c5f6f9c87928622a424990c33",
+    "url": "/sequencer/static/media/Chord1.3ec5329c.mp3"
+  },
+  {
+    "revision": "cc36e9c7c04076970b10c78494d23eb5",
+    "url": "/sequencer/static/media/Chord2.cc36e9c7.mp3"
+  },
+  {
+    "revision": "7614c9e3feafdffe00089f803c8102a8",
+    "url": "/sequencer/static/media/Chord3.7614c9e3.mp3"
+  },
+  {
+    "revision": "4b3a2235efe535ae72a67b659e589aac",
+    "url": "/sequencer/static/media/Chord4.4b3a2235.mp3"
+  },
+  {
+    "revision": "9cb8458f9816f60e3e518232625c5119",
+    "url": "/sequencer/static/media/SFX.9cb8458f.mp3"
+  },
+  {
+    "revision": "b7bbbe8346fcad3d8c7c44f71c1d2970",
+    "url": "/sequencer/index.html"
+  }
+];
